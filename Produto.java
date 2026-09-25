746d63ad620486395ff471cb820d3f862ddb0079
𝄞 Malu <maluiza.ittner@gmail.com>
2026-09-25 19:58:35 -0300

    Refactor Produto class with corrections and improvements
    Corrected variable names, added missing methods, and fixed syntax errors.

--- Produto.java
+++ Produto.java
@@ -1,40 +1,60 @@
 public class Produto {
     private int id;
-    private String;
+    private String nome;
     private double preco;
-    private int quantEstoque;
+    private int quantidadeEstoque;
 
-    public Produto(int id,String nome, double preco, int quantEstoque){
+    public Produto(){}
+
+    public Produto(int id, String nome, double preco, int quantidadeEstoque) {
         this.id = id;
-        this.nome =  nome;
+        this.nome = nome;
         this.preco = preco;
-        this.quantEstoque = quantEstoque;
+        this.quantidadeEstoque = quantidadeEstoque;
     }
 
     public void adicionar(int quantidade){
-        quantstoque += quantidade;
+        quantidadeEstoque += quantidade;
     }
 
     public void remover(int quantidade){
-        quantEstoque -= quantidade;
+        quantidadeEstoque -= quantidade;
     }
 
-    public void  exibirDados(){
-        Sysytem.out.println("ID " + id);
-        System.out.println("Nome " + nome);
-        System.out.println("Preço R$" + preco);
-        System.out.println("Quantidade em estoque " + quantidade);
+    public void exibirDados(){
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Quantidade em estoque: " + quantidadeEstoque);
     }
+
     public int getId() {
         return id;
     }
-
-    public void setId(int id){
-        this.id
+    public void setId(int id) {
+        this.id = id;
     }
-    public double getPreco(){
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
         return preco;
     }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
 
-    public void setQuantidade
+    @Override
+    public String toString(){
+        return "ID: " + id + " | Nome: " + nome + " | Preço: R$" + preco + " | Quantidade em estoque: " + quantidadeEstoque;
+    }
 }
